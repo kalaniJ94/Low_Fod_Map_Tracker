3e648fce97a2bf2dbe41214784fe1f144b1eb3c8
kalaniJ94 <kalaniJ94@gmail.com>
2024-01-09 09:04:52 -0700

    mongoDB connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const {connect, connection } = require('mongoose');
 
 const connectionString =
 //  process.env.MONGODB_URI || 
-"mongodb://mongodb://127.0.0.1:27017/lfm-tracker";
+"mongodb://127.0.0.1:27017/lfm-tracker";
 
 connect(connectionString);
 
